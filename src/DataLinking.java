27a36cf1b6e2eef84cd0da0f0859e73c37a4ed75
wilfriedzakie <zakie.willy@gmail.com>
2018-01-03 16:08:17 +0100

    Main class

---
+++ src/DataLinking.java
@@ -0,0 +1,13 @@
+
+public class DataLinking {
+	
+	
+	public static void main(String[] args)
+	{
+		String file=args[0];
+		String directory=args[1];
+		Dataset d=TDBFactory.createDataset(directory);
+		
+		
+	}
+}
