9781bd21ea107e16e6110a1b70770116fd0cdc77
wilfriedzakie <zakie.willy@gmail.com>
2018-01-15 14:00:43 +0100

    old

--- src/DataLinking.java
+++ src/DataLinking.java
@@ -15,49 +15,12 @@ import org.apache.jena.tdb.TDBFactory;
 
 //change
 public class DataLinking {
-	
-	
-	
-	public static String alignment(String data)
-	{
-		Dataset d2 = TDBFactory.createDataset("D://Informatiion integration//PR-1/person1/db_person2\"");
-		String sparqlQueryString2 = " PREFIX inst: <http://www.okkam.org/ontology_person2.owl#> SELECT * WHERE { ?p inst:soc_sec_id '"+ data + "' .}";
-		org.apache.jena.query.Query query2 = QueryFactory.create(sparqlQueryString2);
-		QueryExecution qexec2 = QueryExecutionFactory.create(query2, d2);
-		System.out.println(sparqlQueryString2);
-
-		String person2=new String();
-		try {
-			ResultSet results2 = qexec2.execSelect();
-			while (results2.hasNext()) {
-
-				QuerySolution sol2 = results2.next();
-				System.out.println(sol2);
-
-				for (Iterator<String> names2 = sol2.varNames(); names2.hasNext();) {
-
-					final String name2 = names2.next();
-
-					if (name2.toLowerCase().contains("p")) {
-						System.out.println("\t"+name2+" := "+sol2.get(name2));
-						person2 = sol2.get(name2).toString();
-					}
-				}
-			}
-		} finally {
-			qexec2.close();
-		}
-		// Close the dataset
-		d2.close();	
-		
-		return person2;
-	}
 
 	public static void main(String[] args) {
 
 		Dataset d = TDBFactory.createDataset("D://Informatiion integration//PR-1/person1/db_person1\"");
 		// Dataset d= data.d1;
-		
+		Dataset d2 = TDBFactory.createDataset("D://Informatiion integration//PR-1/person1/db_person2\"");
 
 		// Read database from location
 		/*
@@ -77,12 +40,10 @@ public class DataLinking {
 
 		String sparqlQueryString1 = "PREFIX humans: <http://www.inria.fr/2007/09/11/humans.rdfs#> PREFIX inst: <http://www.inria.fr/2007/09/11/humans.rdfs-instances#> SELECT ?x WHERE {?x humans:hasSpouse inst:Catherine .}";
 
-		//String sparqlQueryString = " PREFIX p1: <http://www.okkam.org/ontology_person1.owl#>"
-			//	+ " PREFIX p2: <http://www.okkam.org/ontology_person2.owl#>" +
-				//" SELECT * WHERE { ?X  p2:soc_sec_id  ?o ."
-				//+ "}";
-		
-		String sparqlQueryString = " PREFIX inst: <http://www.okkam.org/ontology_person1.owl#> SELECT * WHERE { ?p inst:soc_sec_id ?o .}";
+		String sparqlQueryString = " PREFIX p1: <http://www.okkam.org/ontology_person1.owl#>"
+				+ " PREFIX p2: <http://www.okkam.org/ontology_person2.owl#>" +
+				" SELECT * WHERE { ?X  p2:soc_sec_id  ?o ."
+				+ "}";
 
 		// String sparqlQueryString2=" PREFIX inst:
 		// <http://www.okkam.org/ontology_person2.owl#> SELECT * WHERE { ?p
@@ -95,45 +56,58 @@ public class DataLinking {
 			while (results.hasNext()) {
 
 				QuerySolution sol = results.next();
-				String person2=new String();
-				String person1=new String();
+
 				System.out.println("Solution := " + sol);
 				for (Iterator<String> names = sol.varNames(); names.hasNext();) {
 
 					final String name = names.next();
-					
+
 					String second_onto = new String();
-					if (name.toLowerCase().contains("o")) {
-						 System.out.println("\t"+name+" := "+sol.get(name));
-						//System.out.println("\t" + sol.get(name));
-						person2 = alignment(sol.get(name).toString());
+					//if (name.toLowerCase().contains("o")) {
+						// System.out.println("\t"+name+" := "+sol.get(name));
+						System.out.println("\t" + sol.get(name));
+
+						String ssn = sol.get(name).toString();
 						
-						
-					}
-					else
-						person1=sol.get(name).toString();
-						
-					//else if(person2.isEmpty())
-					//System.out.println("\t ontology1"+name+" := "+sol.get(name)+"   ontology2:="+person2);
-					
-					
 
 						//String sparqlQueryString2 = " PREFIX inst: <http://www.okkam.org/ontology_person2.owl#> SELECT * WHERE { ?p inst:soc_sec_id "
 							//	+ ssn + "  .}";
 						
-					
+					//	System.out.println(sparqlQueryString2);
 
+						/*org.apache.jena.query.Query query2 = QueryFactory.create(sparqlQueryString2);
+						QueryExecution qexec2 = QueryExecutionFactory.create(query2, d2);
+						try {
+							ResultSet results2 = qexec2.execSelect();
+							while (results2.hasNext()) {
+
+								QuerySolution sol2 = results2.next();
+
+								for (Iterator<String> names2 = sol2.varNames(); names2.hasNext();) {
+
+									final String name2 = names2.next();
+
+									if (name.toLowerCase().contains("x")) {
+										// System.out.println("\t"+name+" := "+sol.get(name));
+										ssn = sol.get(name2).toString();
+									}
+								}
+							}
+						} finally {
+							qexec2.close();
+						}
+						// Close the dataset
 						
+					}*/
 					//else
-						 
+						 System.out.println("\t ontology1"+name+" := "+sol.get(name)+"   ontology2:="+second_onto);
 				}
-				System.out.println("Person1: "+person1+"\nPerson2: "+person2);	
 			}
 		} finally {
 			qexec.close();
 		}
 		// Close the dataset
-		
+		//d2.close();
 		d.close();
 
 	}
